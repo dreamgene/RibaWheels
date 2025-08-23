3b05902574c2407b8f0bff51bc703fcdfc9d455a
Ngbede Olofu <2ngbedeolofu@gmail.com>
2025-08-23 16:29:57 +0100

    Remove all old frontend files

--- js/nav-toggle.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('navbar-toggler-custom').addEventListener('click', function() {
-        const mobileMenu = document.getElementById('mobile-menu');
-        mobileMenu.classList.toggle('hidden');
-    });
